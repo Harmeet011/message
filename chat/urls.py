718de3cb75ebbd9a503176876dc2647ec1e0c6e0
Harmeet <hsdclashofclans@gmail.com>
2024-07-18 11:23:51 +1200

    added users viewset

--- chat/urls.py
+++ chat/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from chat.views import sumNumbersView
-from chat.viewsets import ChatRoomViewSets, MessageViewSets
+from chat.viewsets import ChatRoomViewSets, MessageViewSets, UserViewSet
 
 router = DefaultRouter()
 router.register("chatroom", ChatRoomViewSets, basename="chatroom")
 router.register('message', MessageViewSets, basename='message')
+router.register(r'users', UserViewSet)
 urlpatterns = router.urls
 urlpatterns += [
     path('sum_numbers/', sumNumbersView, name='sum_numbers'),
