8daea384169651a465d2a0eb7a6bfd14515f2a50
Harmeet <hsdclashofclans@gmail.com>
2024-07-18 20:12:48 +1200

    register new user done

--- chat/serializers.py
+++ chat/serializers.py
@@ -25,6 +25,7 @@ class UserSerializer(serializers.ModelSerializer):
             username=validated_data['username'],
             email=validated_data['email']
         )
+        user = User.objects.create_user(username=validated_data['username'], email=validated_data['email'])
         user.set_password(validated_data['password'])
         user.save()
         return user
